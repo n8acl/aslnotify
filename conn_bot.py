54858da75cc9f16af49a40188d90faec7bdee9d4
ScottDIrwin <100966715+ScottDIrwin@users.noreply.github.com>
2022-08-17 12:27:22 -0400

    Update conn_bot.py
    Added condition to surprise private nodes.
    Added code to use astdb.txt to get node information and build a variable for it.
    Modified message to include call and location of Node.  This can be customized to add any other column from astdb.txt.

--- conn_bot.py
+++ conn_bot.py
@@ -1,5 +1,6 @@
 # Import Libraries
 import config
+import csv
 import sys
 import os
 import json
@@ -12,6 +13,9 @@ status = "ALLSTAR ALERT: "
 my_node = sys.argv[2]
 their_node = sys.argv[3]
 conn_status = sys.argv[1]
+node_db = "/etc/asterisk/astdb.txt"
+db_deliminater = "|"
+node_info = ""
 
 def send(status):
     if config.publish_telegram: # Send telegram message
@@ -30,7 +34,24 @@ def send(status):
             "user": config.pushover_user,
             "message": status,
             }), { "Content-type": "application/x-www-form-urlencoded" })
-        conn.getresponse()  
+        conn.getresponse()
+
+# Build Node Information
+# Open Node Database
+csvfile = open(node_db, "r", encoding="Latin-1")
+csvfile.seek
+reader = csv.reader(csvfile, dialect='excel', delimiter=db_deliminater, quotechar="'")
+
+# Search cvs for the node string
+
+for line in reader:
+    if their_node in line:
+      node_info = " (" + line[1] + " in " + line[3] + ")"
+    else:
+      node=info = " (Info not in db)"
+
+csvfile.close()
+
 
 # Check if their node is a blocked node and if it is disconnect it automatically and send a message that was disconnected
 # Otherwise, send it through the notificaton control code.
@@ -41,18 +62,18 @@ if (their_node in config.blocked_nodes_list) and (my_node not in config.other_no
     os.system(cmd)
 
     # Notify me that it was auto disconnected
-    status = status + "Blocked node " + str(their_node) + " was auto disconnected from " + str(my_node) + "."
+    status = status + "Blocked node " + str(their_node) + str(node_info) + " was auto disconnected from " + str(my_node) + "(Technology First Hub)."
 
     # send message
     send(status)
 else:
     #build connect/disconnect message
-    if (their_node not in config.my_nodes_list):
-        status = status + "Node " + str(their_node) 
+    if (their_node not in config.my_nodes_list) and (their_node not in config.private_nodes_list):
+        status = status + "Node " + str(their_node) + str(node_info)
         if int(conn_status) == 1:
-            status = status + " connected to "
+            status = status + " connected to " + str(my_node) + " (Technology First Hub)."
         else:
-            status = status + " disconnected from "
+            status = status + " disconnected from " + str(my_node) + " (Technology First Hub)."
 
         # send message
         send(status)
