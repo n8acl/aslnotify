2ade9147bae3eced1b7e5da0e81ec8ee14ed1fe6
ScottDIrwin <100966715+ScottDIrwin@users.noreply.github.com>
2022-08-18 14:30:00 -0400

    Update config.py
    Updated to include db delaminater (this variable was in conn_bot.py in last release).

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 
 # For full explanation of these variables, please see the Configuration section of the Repo.
 
-my_nodes_list = ["12345","54321"] # This is the list of your nodes
-private_nodes_list = ["1999","1998"] # This is a list of the private nodes you connect to your node. This would be like Bridges.
-other_nodes_list = ["67890", "67891"] # This is a list of other nodes you host, like a clubs node, on the same server
+my_nodes_list = ["54321","12345"] # This is the list of your nodes
+private_nodes_list = ["1998","1997"] # This is a list of the private nodes you connect to your node. This would be like Bridges.
+other_nodes_list = ["56789"] # This is a list of other nodes you host, like a clubs node, on the same server
 blocked_nodes_list = ["99999"] # These are nodes that you want to block from accessing any of your nodes.
 echolink_node = 1999 # This is your echolink node if you are using Echolink.
 node_db = "/etc/asterisk/astdb.txt" # This is the location of the astdb.txt database. This could be in your Allmon2 directory or in your Asterisk Directory
+db_deliminater = "|" # This is the deliminater used in the astdb.txt file.
 
 # Telegram Configuration
 publish_telegram = False # Enable/Disable Telegram Bot Notification
@@ -17,8 +18,8 @@ telegram_keys = { # These are the Bot API keys.
 }
 
 # Discord Configuration
-publish_discord = False # Enable/Disable Discord Notifiations
-discord_wh_url = ['DISCORD WEBHOOK URL1 HERE','DISCORD WEBHOOK URL2 HERE'] # Your Discord Webhook for the channel you are connecting to.
+publish_discord = True # Enable/Disable Discord Notifiations
+discord_wh_url = ['YOUR DISCORD HOOK HERE','YOUR SECOND DISCORD HOOK HERE'] # Your Discord Webhook for the channel you are connecting to.
 
 # Pushover configuration
 publish_pushover = False # Enable or disable notifications via Pushover
